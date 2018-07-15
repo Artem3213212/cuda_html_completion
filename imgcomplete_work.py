afe85c4fdf002d9cb4421299f0d2b676ed9de8ce
Artem3213212 <cx-1212@yandex.ru>
2018-07-15 21:46:29 +0300

    Fix #10

--- imgcomplete_work.py
+++ imgcomplete_work.py
@@ -1,5 +1,8 @@
 import os,re
         
+REGEX_PICS='.*\\.(png|bmp|gif|ico|jpg|jpeg)'
+REGEX_SRC='.*<\\s*img\\s+(.*\\s+|)src='
+        
 def get_folder_items(path,old_path,reg):
     s=os.path.dirname(old_path)
     start=os.path.basename(old_path)
@@ -11,18 +14,23 @@ def get_folder_items(path,old_path,reg):
     s=''
     if folder=='':
         folder='.'
+    lsD=[]
+    lsF=[]
     for i in os.scandir(folder):
         if not i.name.startswith(start):
             continue
         if i.is_dir():
-            s=s+'|'+i.name+os.path.sep+chr(13)
-        else:
-            if re.fullmatch(reg,i.name):
-                s=s+'|'+i.name+chr(13)
+            lsD.append(i.name) 
+        elif re.fullmatch(reg,i.name,re.I):
+            lsF.append(i.name)
+    lsD.sort()
+    lsF.sort()
+    for i in lsD:
+        s=s+'|'+i+os.path.sep+chr(13)
+    for i in lsF:
+        s=s+'|'+i+chr(13)
     if('..'+os.path.sep).startswith(start):
         s='|..'+os.path.sep+chr(13)+s
-    if('.'+os.path.sep).startswith(start):
-        s='|.'+os.path.sep+chr(13)+s
     return [s,z]
     
 def imgcomplete_on_complete(ed):
@@ -32,22 +40,20 @@ def imgcomplete_on_complete(ed):
     x,y,x1,y2=carets[0]
     s=ed.get_text_line(y)[:x]
     file_dir=os.path.dirname(ed.get_filename())
-    if re.fullmatch('.*<\\s*img\\s+(.*\\s+|)src="[^"]*',s):
-    #if re.fullmatch('.*<img\\s+([^<>]*\\s+|)src="[^\\"]*',s):
+    if re.fullmatch(REGEX_SRC+'"[^"]*',s):
         try:
             for i in range(len(s)-1,-1,-1):
                 if s[i]=='"':
-                    temp=get_folder_items(file_dir,s[i+1:],'.*\\.(png|bmp|gif|ico|jpg|jpeg|PNG|BMP|GIF|ICO|JPG|JPEG)')
+                    temp=get_folder_items(file_dir,s[i+1:],REGEX_PICS)
                     ed.complete(temp[0],temp[1],0)
                     return True
         except:
             pass
-    if re.fullmatch(".*<\\s*img\\s+(.*\\s+|)src='[^']*",s):
-    #if re.fullmatch(".*<img\\s+([^<>]*\\s+|)src='[^\\']*",s):
+    if re.fullmatch(REGEX_SRC+"'[^']*",s):
         try:
             for i in range(len(s)-1,-1,-1):
                 if s[i]=="'":
-                    temp=get_folder_items(file_dir,s[i+1:],'.*\\.(png|bmp|gif|ico|jpg|jpeg|PNG|BMP|GIF|ICO|JPG|JPEG)')
+                    temp=get_folder_items(file_dir,s[i+1:],REGEX_PICS)
                     ed.complete(temp[0],temp[1],0)
                     return True
         except:
