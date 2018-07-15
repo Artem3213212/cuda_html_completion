97dfbc152883aa9493e30e3af07de1fdc0182638
Artem3213212 <cx-1212@yandex.ru>
2018-07-15 16:41:08 +0300

    Some updated

--- imgcomplete_work.py
+++ imgcomplete_work.py
@@ -1,21 +1,10 @@
 import os,re
 
-def get_dir(s):
-    for i in range(len(s)-1,-1,-1):
-        if s[i]=='\\'or s[i]=='/' or s[i]=='"':
-            return s[:i]
-    return ''
         
-def get_floader_items(path,old_path,reg):
-    z=len(old_path)
-    s=get_dir(old_path)
-    z=z-len(s)
-    if old_path[-z]=='\\'or old_path[-z]=='/' or old_path[-z]=='"':
-        z=z-1
-    if z==0:
-        start=''
-    else:
-        start=old_path[-z:]
+def get_folder_items(path,old_path,reg):
+    s=os.path.dirname(old_path)
+    start=os.path.basename(old_path)
+    z=len(start)
     if s=='':
         flod=path
     else:
@@ -43,14 +32,14 @@ def imgcomplete_on_complete(ed):
         return False
     x,y,x1,y2=carets[0]
     s=ed.get_text_line(y)[:x]
-    file_dir=get_dir(ed.get_filename())
-    if re.match('.*<\\s*img\\s+(.*\\s+|)src="[^"]*',s):
-        try:
+    file_dir=os.path.dirname(ed.get_filename())
+    if re.match('''.*<\\s*img\\s+(.*\\s+|)src="[^"]*''',s):
+        #try:
             for i in range(len(s)-1,-1,-1):
                 if s[i]=='"':
-                    temp=get_floader_items(file_dir,s[i+1:],'.*\\.(png|bmp)')
+                    temp=get_folder_items(file_dir,s[i+1:],'.*\\.(png|bmp)')
                     ed.complete(temp[0],temp[1],0)
                     return True
-        finally:
-            pass
+        #except:
+            #pass
     return False
