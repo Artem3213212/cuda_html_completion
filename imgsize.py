56e48ac1f963c83f4e81818e292737435de83ba7
Artem3213212 <cx-1212@yandex.ru>
2018-07-18 17:45:38 +0300

    Some updated

--- imgsize.py
+++ imgsize.py
@@ -34,6 +34,18 @@ def get_image_size(fname):
             height, width = struct.unpack('>HH', fhandle.read(4))
         except Exception: #IGNORE:W0703
             return
+    elif imghdr.what(fname)=='bmp': 
+        fhandle.seek(18)
+        width=int.from_bytes(fhandle.read(4),byteorder='little')
+        height=int.from_bytes(fhandle.read(4),byteorder='little')
+    elif fname.endswith('.ico'):
+        fhandle.seek(6)
+        width=int.from_bytes(fhandle.read(1),byteorder='little')
+        if width==0:
+            width=256
+        height=int.from_bytes(fhandle.read(1),byteorder='little')     
+        if height==0:
+            height=256   
     else:
         return
     return width, height
